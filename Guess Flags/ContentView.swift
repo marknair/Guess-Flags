8d1541ca0e29ec1ec10ed96a6c638b72cbd074bd
Mark <marknair@me.com>
2026-04-23 09:19:59 -0500

    Initial Commit

---
+++ Guess Flags/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guess Flags
+//
+//  Created by Mark Nair on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guess Flags/Guess_FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_FlagsApp.swift
+//  Guess Flags
+//
+//  Created by Mark Nair on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Guess_FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
